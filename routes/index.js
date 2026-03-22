cf0904aa1ba42fb2a77022994a1116d1dbe32c48
Said Mohamed <tt7377ml@go.minneapolis.edu>
2026-03-22 16:55:18 -0500

    For the recipe app...

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,26 @@ const express = require('express')
 const router = express.Router()
 const generateRecipe = require('../services/generate_recipe')
 
+
+// this will focus on the homepage.
 router.get('/', function(req, res, next){
     res.render('enter_ingredients')
 })
 
+
+// this will receive the information and request.
 router.post('/generate_recipe', function(req, res, next){
-    // TODO complete this method 
+    let formData = req.body
+    let userIngredients = formData.ingredients
+    console.log(`User entered:`, userIngredients)
+
+    // this will make a Gemini request.
+    generateRecipe(userIngredients).then( recipeJSON => {
+        return res.render('recipe_result', { userIngredients: userIngredients, recipeJSON: recipeJSON })
+    }).catch(err => { // this will hand off the request to the error handler.
+        return next(err)
+    })
+
 })
 
 module.exports = router
--- services/generate_recipe.js
+++ services/generate_recipe.js
@@ -1,3 +1,63 @@
-// TODO complete this file
+// this will request the LLM.
+let { GoogleGenAI, Type } = require('@google/genai') // we added the TYPE to the required gemini AI tool.
 
+let genAI = new GoogleGenAI( {} )
 
+function generateRecipe(ingredients) {
+
+    let prompt = `Suggest one recipe that uses these ingredients. Ingredients: ${ingredients}`
+
+// we are returning to the generated content.
+    return genAI.models.generateContent({
+        model: 'gemini-2.5-flash', // this will be the model that will be used from Gemini.
+        contents: prompt, // this is from the previous string above.
+        config: {
+            systemInstructions: `You are a recipe suggestion bot for a health-conscious, budget-friendly website. 
+            Suggest recipes that are low cost but use healthy ingredients. 
+            Only edible food ingredients please. 
+            Please display a warning to the consume for non-food items. 
+            Alert the consume for any dietary allergens. 
+            Do not suggest the following options: Non-food items, tools, imagination creation or anything malicious. `,
+            responseMimeType: 'application/json', // this will response in a JSON format.
+            responseSchema: {
+                type: Type.OBJECT, // this will focus on the type.
+                properties: { // the properties will provide a structure to the prompt and terminal below.
+                    recipeName: { // this will provide the recipe name on the terminal.
+                        type: Type.STRING
+                    },
+                    description: {
+                        type: Type.STRING
+                    },
+                    nutritionalFacts: {
+                        type: Type.STRING
+                    },
+                    allergens: {
+                        type: Type.STRING
+                    },
+                    ingredients: { // this will provide the ingredients in an array and string.
+                        type: Type.ARRAY,
+                        items: {
+                            type: Type.STRING
+                        }
+                    },
+                    instructions: { // this will provide the array and string for the instructions.
+                        type: Type.ARRAY,
+                        items: {
+                            type: Type.STRING
+                        }
+                    }
+                } // so many object and curvy braces in this code.
+
+            }
+        }// this will provide the contents that you want to display on the AI prompt.
+    }).then(response => {
+        console.log(response.text)
+        let recipe = JSON.parse(response.text)
+        console.log(recipe)
+        return recipe
+        //let recipe = JSON.parse(response.text) // this will parse through the response.
+    }) // this is an example of prompt engineering.
+
+}
+// this will export the generate recipe on the webpage.
+module.exports = generateRecipe
